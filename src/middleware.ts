1590fd729ed62c04c960a012d713557c805b8ca1
Alex Olsen <aotech@ao.local>
2024-03-19 08:43:14 -0700

    not fixing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/', "/api/webhooks(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
  
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
